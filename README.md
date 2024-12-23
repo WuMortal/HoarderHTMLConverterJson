04ecb49878e71b94d039dd0e19205893dac21d1d
Wigor <wumeng@wondersgroup.com>
2024-12-23 18:03:59 +0800

    dev:convert hoarder json

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 ## Use
-![favorites](./favorites.png)
+
+[WebSite](https://wumortal.github.io/HoarderHTMLConverterJson/)
+
+1. export `Chrome` `Edge` bookmark
+
+![favorites](./document/bookmark.png)
+
+2. select bookmark `HTML` File
+
+![select](./document/select.png)
+
+3. click `Convert` button
+
+![convert](./document/convert.png)
+
+4. click `download hoarder json` button
+
+![download](./document/download.png)
+
+5. import `hoarder_xxx.json` to `Hoarder`
+
+![import](./document/import.png)
+
+## Development
+
+clone `repo` and open `visual studio code`
--- index.js
+++ index.js
@@ -1,9 +1,61 @@
+//https://github.com/WuMortal/HoarderHTMLConverterJson
+
 $(function () {
 
+    var htmlText = '';
     var parseData = $([]).toArray();
+    var hoarderData = {};
 
-    let rootFavorites = $('body > dl > dt > dl > dt > h3');
-    recursiveParse(rootFavorites);
+    $('#app-content-file-select').change(function () {
+        if (this.files.length <= 0) {
+            $('.app-content-result-download').hide();
+            $('.app-content-file-parse').attr('disabled', true);
+            return;
+        }
+        let file = this.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                htmlText = e.target.result;
+                if (htmlText.indexOf('<!DOCTYPE NETSCAPE-Bookmark-file-1>') <= -1) {
+                    $('.app-content-result-text').val('html file is not valid!');
+
+                    $('.app-content-file-parse').attr('disabled', true);
+                    return;
+                }
+                $('.app-content-file-parse').attr('disabled', false);
+                $('.app-content-result-text').val('html is valid,please click [Convert] button.');
+            };
+            reader.readAsText(file);
+        }
+
+    });
+
+    $('.app-content-file-parse').click(function () {
+        if (htmlText == null || htmlText == '') {
+            $('.app-content-result-text').val('not found file!');
+            $('.app-content-result-download').hide();
+            return;
+        }
+
+        $('.app-content-file-parse').attr('disabled', true);
+
+        const html = $.parseHTML(htmlText);
+        let rootFavorites = $(html).children('dt').children('dl').children('dt').children('h3');
+        recursiveParse(rootFavorites);
+        hoarderData = convertToHoarder(parseData);
+        $('.app-content-result-text').val(JSON.stringify(hoarderData, null, 4));
+
+        $('.app-content-result-download').show();
+        $('.app-content-file-parse').attr('disabled', false);
+    });
+
+    $('.app-content-result-download').click(function () {
+        var a = document.createElement('a');
+        a.download = `hoarder_${new Date().getTime()}.json`;
+        a.href = URL.createObjectURL(new Blob([JSON.stringify(hoarderData, null, 4)], {type: "application/json"}));
+        a.click();        
+    });
 
     function recursiveParse(folderTag, parentFolderPath) {
         $.each(folderTag, (i, e) => {
@@ -27,8 +79,26 @@ $(function () {
         });
     }
 
-    function convertToHoarderJson(data) {
-        //TODO:
-    }
+    function convertToHoarder(data) {
+        if (data.length <= 0) {
+            return [];
+        }
 
+        var bookmarks = data.map(e => {
+            return {
+                'createdAt': Math.floor(new Date().getTime() / 1000),
+                'title': e.title,
+                'tags': e.path ? e.path.split(',') : [],
+                "content": {
+                    "type": "link",
+                    "url": e.url
+                },
+                "note": null
+            };
+        });
+
+        return {
+            "bookmarks": bookmarks
+        }
+    }
 });
